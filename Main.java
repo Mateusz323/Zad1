2dcbbf746deb8440436ce188555e663bd706281b
Mateusz323 <45674849+Mateusz323@users.noreply.github.com>
2018-12-07 16:16:40 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,38 +4,61 @@ public class Main {
    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
    static final String DB_URL = "jdbc:mysql://10.0.10.3:3306/myDBChwalczuk";
 
-   static final String USER = "user";
-   static final String PASS = "pass123";
+   static final String USER = "MChwalczuk";
+   static final String PASS = "password";
    
    public static void main(String[] args) {
    Connection conn = null;
    Statement stmt = null;
+   Boolean baseExist = false;
+   String sql;
+  
    try{
-      Class.forName("com.mysql.jdbc.Driver");
-
+      Class.forName("com.mysql.cj.jdbc.Driver");
+	   
       System.out.println("Connecting to database...");
-      conn = DriverManager.getConnection(DB_URL,USER,PASS);
-
+      conn = DriverManager.getConnection(DB_URL,USER,PASS);	   
+      	   
+      System.out.println("Check if table in base exist");
+      DatabaseMetaData md = conn.getMetaData();
+      ResultSet rs = md.getTables(null, null, "Persons", null);
+      while (rs.next()) {
+            System.out.println("Table Exist");
+	    baseExist = true;
+      }
+      rs = null;  
+      if(!baseExist){
+      	System.out.println("Creating Table");
+      	stmt = conn.createStatement();
+      	sql = "CREATE TABLE Persons (PersonID int, LastName varchar(255), FirstName varchar(255), Address varchar(255), City varchar(255) )";
+      	stmt.executeUpdate(sql);
+      	stmt = null;
+      }
+	   
       stmt = conn.createStatement();
-      String sql1, sql2, sql3;
-      sql1 = "CREATE TABLE Persons (PersonID int, LastName varchar(255), FirstName varchar(255))";
-      sql2 = "INSERT INTO Persons (PersonID, LastName, FirstName) VALUES (1, 'Mateusz', 'Chwalczuk')";
-      sql3 = "SELECT ID, FirstName, LastName FROM Persons";
+      System.out.println("Inserting Data to Table");
+      sql = "INSERT INTO Persons (PersonID, LastName, FirstName, Address, City) VALUES (1, 'Nazwisko1', 'Imie1', 'Ulica1','Lublin'), (2, 'Nazwisko2', 'Imie2', 'Ulica2','Lublin'), (3, 'Nazwisko3', 'Imie3', 'Ulica3','Lublin')";
+      stmt.executeUpdate(sql);	 
+      stmt = null;
+	   
+      stmt = conn.createStatement();
+      sql = "SELECT PersonID, FirstName, LastName, Address, City FROM Persons";
+      rs = stmt.executeQuery(sql);
 
-      ResultSet rs1 = stmt.executeQuery(sql2);
-      ResultSet rs2 = stmt.executeQuery(sql3);
-      ResultSet rs3 = stmt.executeQuery(sql3);
+      while(rs.next()){
+         int id  = rs.getInt("PersonID");
+         String first = rs.getString("FirstName");
+         String last = rs.getString("LastName");
+	 String address = rs.getString("Address");
+	 String city = rs.getString("City");
 
-      while(rs3.next()){
-         int id  = rs3.getInt("PersonID");
-         String first = rs3.getString("FirstName");
-         String last = rs3.getString("LastName");
-		 
          System.out.println("ID: " + id);
          System.out.println(", First: " + first);
          System.out.println(", Last: " + last);
+	 System.out.println(", Address: " + address);
+	 System.out.println(", City: " + city);
       }
-      rs3.close();
+      rs.close();
       stmt.close();
       conn.close();
    }catch(SQLException se){
